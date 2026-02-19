066583c29754ca53013f4959f61348e7fb8aa70b
abhishekumarkar01-afk <abhishekumarkar01@gmail.com>
2026-02-19 14:10:12 +0530

    Delete app/api/v1/crop.py

--- app/api/v1/crop.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1 import crop, soil, farm
-
-api_router = APIRouter()
-api_router.include_router(crop.router, prefix="/crop", tags=["Crop"])
-api_router.include_router(soil.router, prefix="/soil", tags=["Soil"])
-api_router.include_router(farm.router, prefix="/farm", tags=["Farm"])
